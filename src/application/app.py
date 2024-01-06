82350eee3db2291baec1bb30b36f265d40781437
jefferson <jeffnacato@gmail.com>
2024-01-06 16:43:34 +0100

    feat: Update image tag and add online check endpoint

--- src/application/app.py
+++ src/application/app.py
@@ -12,6 +12,7 @@ app = FastAPI()
 REQUESTS = Counter('server_requests_total', 'Total number of requests to this webserver')
 HEALTHCHECK_REQUESTS = Counter('healthcheck_requests_total', 'Total number of requests to healthcheck')
 MAIN_ENDPOINT_REQUESTS = Counter('main_requests_total', 'Total number of requests to main endpoint')
+ONLINE_CHECK = Counter('online_requests_total', 'Total number of requests to online endpoint')
 
 class SimpleServer:
     """
@@ -48,8 +49,8 @@ class SimpleServer:
         return {"msg": "Hello World"}
     
     @app.get("/online")
-    async def status_check():
-        """Implement health check endpoint"""
+    async def online_check():
+        """Implement online check endpoint"""
         # Increment counter used for register the total number of calls in the webserver
         REQUESTS.inc()
         # Increment counter used for register the requests to healtcheck endpoint
